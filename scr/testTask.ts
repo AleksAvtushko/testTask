06ff133af1b4bc5f620b7fab7bf425c90fa173c9
Aleksander Avtushko <aleksandravtusko_gmail_com>
2023-01-10 21:47:31 +0300

    feat: test Task

--- scr/testTask.ts
+++ scr/testTask.ts
@@ -1,13 +1,16 @@
 const fizzBuzz = (n: number) => {
-    if (n % 3 === 0) {
+    if (n % 3 === 0 && n % 5 !== 0) {
         return "fizz";
-    } else if (n % 5 === 0) {
+    } else if (n % 5 === 0 && n % 3 !== 0) {
         return "buzz";
-    } else {
+    } else if (n % 3 === 0 && n % 5 === 0) {
         return "fizzBuzz";
+    } else {
+        return n;
     }
 };
 
-console.log(fizzBuzz(61));
-console.log(fizzBuzz(6));
 console.log(fizzBuzz(5));
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(14));
+console.log(fizzBuzz(6));
